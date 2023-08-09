0ce57dfec82dd8f75e13fa0b6ad8a9f892f0c579
makaxi <kamama919@gmail.com>
2023-08-08 23:31:48 -0700

    Create reviews and review react pages

---
+++ src/components/reviews/Reviews.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
+import Review from '../review/Review';
+import "./Reviews.scss";
+
+const Reviews = ({gigId}) => {
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['reviews'],
+    queryFn: () =>
+      newRequest.get(
+        `/reviews/${gigId}`)
+        .then((res) => {
+          return res.data
+        }),
+  });
+
+  return (
+    <div className="reviews">
+      <h2>Reviews</h2>
+      {isLoading 
+        ? "Loading..." 
+        : error 
+        ? "Something went wrong" 
+        : data.map((review) => <Review key={review._id} review = {review} />)
+      }
+    </div>
+  )
+}
+
+export default Reviews
