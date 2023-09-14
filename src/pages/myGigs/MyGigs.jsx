07cf65676a897b5059a9d9df1119630275c2490c
makaxi <kamama919@gmail.com>
2023-09-14 16:51:55 -0700

    myGigs page is now dynamic, displays all the gigs the seller has

--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -1,48 +1,87 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useQueryClient, useQuery, useMutation } from "@tanstack/react-query";
 import "./MyGigs.scss";
+import newRequest from "../../utils/newRequest";
 import getCurrentUser from "../../utils/getCurrentUser";
 
 function MyGigs() {
   const currentUser = getCurrentUser();
+  console.log(currentUser);
 
+  const queryClient = useQueryClient();
+
+  const {isLoading, error, data} = useQuery({
+    queryKey: ["myGigs"],
+    queryFn: () =>
+      newRequest.get(`/gigs?userId=${currentUser._id}`).then((res) => {
+        return res.data;
+      }),
+  });
+
+  const mutation = useMutation({
+    mutationFn: (id) => {
+      newRequest.delete(`/gigs/${id}`)
+    },
+    onSuccess:() => {
+      queryClient.invalidateQueries(["myGigs"]);
+    },
+  });
+
+  const handleDelete = (id) => {
+    e.preventDefault();
+    mutation.mutate(id); 
+  }
+
+  console.log(data);
 
   return (
     <div className="myGigs">
-      <div className="container">
-        <div className="title">
-          <h1>{currentUser.isSeller ? "Gigs" : "Orders"}</h1>
-          {currentUser.isSeller && (
-            <Link to="/add">
-              <button>Add New Gig</button>
-            </Link>
-          )}
+      {isLoading 
+        ? "Loading" 
+        : error 
+        ? "Something went wrong"
+        : <div className="container">
+            <div className="title">
+              <h1>Gigs</h1>
+              {currentUser.isSeller && (
+                <Link to="/add">
+                  <button>Add New Gig</button>
+                </Link>
+              )}
+            </div>
+            <table>
+              <thead>
+                <tr>
+                  <th>Image</th>
+                  <th>Title</th>
+                  <th>Price</th>
+                  <th>Sales</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              {data.map((gig) => (
+                <tbody>
+                  <tr key={gig._id}>
+                    <td>
+                      <img
+                        className="image"
+                        src={gig.cover}
+                        alt=""
+                      />
+                    </td>
+                    <td>{gig.title}</td>
+                    <td>{gig.price}</td>
+                    <td>{gig.sales}</td>
+                    <td>
+                      <img className="delete" src="./img/delete.png" alt="" onClick={()=>handleDelete(gig._id)}/>
+                    </td>
+                  </tr>
+                </tbody>
+              ))}
+            </table>
         </div>
-        <table>
-          <tr>
-            <th>Image</th>
-            <th>Title</th>
-            <th>Price</th>
-            <th>Sales</th>
-            <th>Action</th>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="image"
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-            </td>
-            <td>Stunning concept art</td>
-            <td>59.<sup>99</sup></td>
-            <td>13</td>
-            <td>
-              <img className="delete" src="./img/delete.png" alt="" />
-            </td>
-          </tr>
-        </table>
-      </div>
+      }
     </div>
   );
 }
